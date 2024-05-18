55817ebfa67d663b105ca3157c3f9ebb9e36264d
Tonny-William <tonnywilliamnasty96@gmail.com>
2024-05-18 17:35:52 +0300

    first commit

---
+++ menu.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+	char SSID;
+	printf("Enter SSID CODE\n");
+	scanf("%s,&SSID");
+	
+	if(SSID=='*148*66#'){
+
+printf("Menu\n");
+printf("1. Yatosha\n");
+printf("2. Kaskazini ofa\n");
+printf("3. sms & dakika\n");
+printf("4. Kimataifa\n");
+printf("5. Angalia salio\n");
+printf("6. Bonyeza '0' kusitisha\n");	
+printf("Enter your choice (1-6)\n");
+	}
+	else{
+		printf("Error 200035698745 occured\n");
+	}
+	
+	int choice;
+	
+	switch(choice){
+		case 1
+		printf("1. siku\n");
+		printf("2. wiki\n");
+		printf("3. mwezi\n");
+		printf("4. mwaka\n");
+		break;
+		
+		case 2
+		printf("1. siku\n");
+		printf("2. wiki\n");
+		printf("3. mwezi\n");
+		printf("4. mwaka\n");
+		break;
+		
+		case 3
+		printf("1. siku\n");
+		printf("2. wiki\n");
+		printf("3. mwezi\n");
+		printf("4. mwaka\n");
+		break;
+		
+		case 4
+		printf("1. siku\n");
+		printf("2. wiki\n");
+		printf("3. mwezi\n");
+		printf("4. mwaka\n");
+		break;
+		
+		case 5
+		int nywila;
+		printf("weka namba ya siri\n");
+		scanf("%d,&nywila");
+		printf("Salio lako ni Tsh.30,000/=\n")
+		break;
+		
+		case 6
+		break;
+		
+		default
+		printf("Huduma hii haipo\n");
+	}
+	
+	reurn 0;	
+}
